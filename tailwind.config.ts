9d246bd718ced08076cfd4eb28c5446119dca469
Franz Muhlhauser <franz.muhlhauser@gmail.com>
2025-10-03 21:38:47 +0000

    puedes hacer que se mueva mas lento

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -112,7 +112,7 @@ export default {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
         'infinite-scroll': 'infinite-scroll 40s linear infinite',
-        'bounce': 'bounce 1s infinite',
+        'bounce': 'bounce 2s infinite',
       },
     },
   },
