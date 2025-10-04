9b65143acfb6219e19b6bef525e1aeb396206aed
Franz Muhlhauser <franz.muhlhauser@gmail.com>
2025-10-04 00:14:21 +0000

    perdon mi error completamente, puedes reemplazar las imagenes por estas:

--- src/components/specialties-section.tsx
+++ src/components/specialties-section.tsx
@@ -58,10 +58,10 @@ export default function SpecialtiesSection() {
                              <div className="w-1/2 mx-auto mb-4">
                                 <div className="grid grid-cols-1 md:grid-cols-2 gap-4 items-center">
                                     <div className="overflow-hidden rounded-lg">
-                                        <Image src="/images/productos/pack_amortiguador_daher.webp" alt="Pack de Amortiguadores Daher" width={600} height={400} className="w-full h-full object-cover rounded-lg transition-all duration-300 hover:scale-110 hover:brightness-110 -rotate-6" />
+                                        <Image src="/images/productos/amortiguadores_gabriel.webp" alt="Amortiguadores Gabriel" width={600} height={400} className="w-full h-full object-cover rounded-lg transition-all duration-300 hover:scale-110 hover:brightness-110 -rotate-6" />
                                     </div>
                                     <div className="overflow-hidden rounded-lg">
-                                        <Image src="/images/productos/amortiguador_daher.webp" alt="Amortiguador Daher" width={600} height={400} className="w-full h-full object-cover rounded-lg transition-all duration-300 hover:scale-110 hover:brightness-110 rotate-6" />
+                                        <Image src="/images/productos/amortiguador_kyb.webp" alt="Amortiguador KYB" width={600} height={400} className="w-full h-full object-cover rounded-lg transition-all duration-300 hover:scale-110 hover:brightness-110 rotate-6" />
                                     </div>
                                 </div>
                             </div>
