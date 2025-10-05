47ae9f24e61b0649f3186f25abad0eeb3d6f8bda
Franz Muhlhauser <franz.muhlhauser@gmail.com>
2025-10-05 23:22:57 +0000

    eliminar (_for element <BrandLogo>_)

--- src/components/specialties-section.tsx
+++ src/components/specialties-section.tsx
@@ -127,9 +127,7 @@ export default function SpecialtiesSection() {
                         <AccordionTrigger onClick={handleTriggerClick} className="text-left text-lg font-semibold hover:text-premium-yellow transition-colors duration-300">Catalíticos</AccordionTrigger>
                         <AccordionContent className="pt-4">
                              <div className="grid md:grid-cols-2 gap-8 items-center">
-                                <div>
-                                    <p className="text-muted-foreground text-justify">Asegure el cumplimiento normativo y la eficiencia de su vehículo con nuestros catalizadores homologados de la marca Gabriel. Fabricados con tecnología de vanguardia, garantizan una reducción óptima de emisiones y un rendimiento excepcional. Adquiera ahora y conduzca con confianza hacia un futuro más limpio. ¡Contáctenos para más información!</p>
-                                </div>
+                                <p className="text-muted-foreground text-justify">Asegure el cumplimiento normativo y la eficiencia de su vehículo con nuestros catalizadores homologados de la marca Gabriel. Fabricados con tecnología de vanguardia, garantizan una reducción óptima de emisiones y un rendimiento excepcional. Adquiera ahora y conduzca con confianza hacia un futuro más limpio. ¡Contáctenos para más información!</p>
                                 <div className="overflow-hidden rounded-lg w-full flex justify-center items-center">
                                     <Image src="/images/productos/catalitico_gabriel.webp" alt="Catalítico Gabriel" width={300} height={200} className="w-2/3 md:w-1/2 h-auto object-cover rounded-lg transition-all duration-300 hover:scale-110 hover:[filter:drop-shadow(0_0_10px_rgba(255,215,0,0.2))]" />
                                 </div>
@@ -141,16 +139,13 @@ export default function SpecialtiesSection() {
                         <AccordionTrigger onClick={handleTriggerClick} className="text-left text-lg font-semibold hover:text-premium-yellow transition-colors duration-300">Amortiguadores para Camiones y Autobuses</AccordionTrigger>
                         <AccordionContent className="pt-4">
                             <div className="grid md:grid-cols-2 gap-8 items-center">
-                                <div>
-                                    <p className="text-muted-foreground text-justify">Repuestos especiales para vehículos de carga y transporte, diseñados para resistir el uso intensivo. Nuestros amortiguadores para camiones y autobuses ofrecen la durabilidad y resistencia necesarias para el transporte comercial y de pasajeros. Garantizamos productos de alta calidad que soportan las exigencias del trabajo pesado.</p>
-                                </div>
+                                <p className="text-muted-foreground text-justify">Repuestos especiales para vehículos de carga y transporte, diseñados para resistir el uso intensivo. Nuestros amortiguadores para camiones y autobuses ofrecen la durabilidad y resistencia necesarias para el transporte comercial y de pasajeros. Garantizamos productos de alta calidad que soportan las exigencias del trabajo pesado.</p>
                                 <div className="overflow-hidden rounded-lg w-full flex justify-center items-center">
-                                    <Image src="/images/productos/amortiguador_camion.webp" alt="Amortiguador de camión" width={200} height={200} className="w-1/2 md:w-1/3 h-auto object-cover rounded-lg transition-all duration-300 hover:scale-110 hover:[filter:drop-shadow(0_0_10px_rgba(255,215,0,0.2))]" />
+                                    <Image src="/images/productos/amortiguador_camion.webp" alt="Amortiguador de camión" width={200} height={200} className="w-1/3 md:w-1/4 h-auto object-cover rounded-lg transition-all duration-300 hover:scale-110 hover:[filter:drop-shadow(0_0_10px_rgba(255,215,0,0.2))]" />
                                 </div>
                             </div>
                              <h4 className="font-semibold text-center text-xl mt-6 mb-4 text-premium-yellow">Marcas Asociadas</h4>
-                            <div className="grid grid-cols-2 gap-4 max-w-sm mx-auto">
-                                <BrandLogo logo={SachsLogo} />
+                            <div className="grid grid-cols-1 gap-4 max-w-sm mx-auto">
                                 <BrandLogo logo={BogeLogo} />
                             </div>
                         </AccordionContent>
@@ -181,6 +176,8 @@ export default function SpecialtiesSection() {
         </section>
     );
 }
+
+    
 
     
 
