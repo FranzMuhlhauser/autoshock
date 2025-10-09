6083af16f67684e8e686730189ee4b0b8459f929
Franz Muhlhauser <franz.muhlhauser@gmail.com>
2025-10-09 14:50:48 +0000

    ahora va muy lento y tiene imagenes sobre puestas

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -111,7 +111,7 @@ export default {
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
-        'infinite-scroll': 'infinite-scroll 40s linear infinite',
+        'infinite-scroll': 'infinite-scroll 25s linear infinite',
         'bounce': 'bounce 2s infinite',
       },
     },
